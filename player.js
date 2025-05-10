2c0e761d8435faad4c12105fe193ec6ee65d3adc
PlazaJose <plaza.jose@correounivalle.edu.co>
2025-05-10 14:18:51 -0500

    not reapeated moves

--- player.js
+++ player.js
@@ -36,10 +36,28 @@ class Jugador{
         }
         //logica de la jugada :v?
         //verificar que no se haya hecho antes
+        const grid_index = this.find_grid_index({row, column});
+        if(grid_index != -1){
+            this.map.moves[grid_index].state = state;
+            return true;
+        }
         //añadir jugada
         this.map.moves.push({row, column, state});
         return true;
     }
+    has_move(new_move) {
+        return this.map.moves.some(move => 
+            move.row === new_move.row &&
+            move.column === new_move.column &&
+            move.state === new_move.state
+        );
+    }
+    find_grid_index(new_move) {
+    return this.map.moves.findIndex(move => 
+        move.row === new_move.row &&
+        move.column === new_move.column
+    );
+}
     player_to_string(){
         return `jugador {id: ${this.id}, name: ${this.name}, mmr: ${this.mmr}}`;
     }
